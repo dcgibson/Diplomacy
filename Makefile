0922a5a4619de3e2fc959f44d411653db937ae82
David Gibson <dgibson314@gmail.com>
2016-07-06 21:54:16 -0400

    Holdbot plus

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: board init engine
+all: board init engine brain
 
 board: board.ml
 	ocamlbuild board.native
@@ -6,6 +6,9 @@ init: init.ml
 	ocamlbuild init.native
 engine: engine.ml
 	ocamlbuild engine.native
+
+brain: brain.ml
+	ocamlbuild brain.native
 
 clean: 
 	rm -rf _build *.native *.cmi *.cmo *.cmx *.o
--- _build/engine.ml
+++ _build/engine.ml
@@ -5,6 +5,7 @@ open Init;;
 (** Prints starting message, and gets country for player.
  * TODO:
      * Random implementation **)
+
 (* Maybe we should have a player list?
  * Have hold_bots and pass the list to
  * game_turn? *)
@@ -21,13 +22,11 @@ open Init;;
 (* This will just return the country that the player chose.
  * Later we'll need to initialize the other players, who
  * the AI program will be acting upon. *)
-let initialize_player bd =
+let init_user bd =
     print_string "~Diplomacy!\n";
-    
-    (* Pass user_country when constructing user player *)
     let init_country () =
         print_string "Choose a country, or type random\n";
-        print_string "Choices: England\n";
+        print_string "Choices: England, France, Germany, Russia, Austria, Turkey, Italy\n";
         let user_country = read_line () in
         match user_country with
         | "England" -> England
@@ -37,13 +36,32 @@ let initialize_player bd =
         | "Austria" -> AH
         | "Turkey" -> Turkey
         | "Italy" -> Italy
+        | "random" | "Random" ->
+                Random.self_init ();
+                let index = Random.int 7 in
+                let countries = [England; France; Germany; AH; Turkey; Italy] in
+                List.nth countries index
         | _ -> failwith "Not a valid country"
     in
     let user_country = init_country () in
+    print_string ((string_of_country user_country) ^ "\n");
+
     {name = user_country;
      supply_centers = sc_ref_list bd user_country;
      force_list = fc_ref_list bd user_country;}
 
+
+let init_AIs bd (lst: country list) =
+    List.fold_left
+        (fun acc c ->
+            let ai =
+                {name = c;
+                 supply_centers = sc_ref_list bd c;
+                 force_list = fc_ref_list bd c;
+                } in
+            ai::acc)
+        []
+        lst
 
 let rec game_turn (bd : board) =
     print_string "Options:\n";
@@ -55,10 +73,17 @@ let rec game_turn (bd : board) =
     match user_option with
     | "B" -> print_string (string_of_board bd); game_turn bd
     | _ -> ()
-(*
-let start_game =
-    (* Initialize the starting board, complete with forces 
-    let game_board = init_forces init_board in *) *)
+
+
+let start_game () =
+
+    let game_board = init_forces init_board in
+    let user = init_user game_board in
+
+    let ai_countries = List.filter (fun x -> x != user.name) [England; France; Germany; Russia; Turkey; AH; Italy] in
+    init_AIs game_board ai_countries
+;;
+
 
 
 
